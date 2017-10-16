b320c5bab0eb9a6d7fe298462ae9f91725fa970d
jiayunzhe <jetic@sjtu.edu.cn>
2017-10-16 18:30:11 +0800

    * Fix assign/zero bug: use @a to replace in functions. * Support variables from A to Z and a to z.

--- jetic_bf/jetic_bf.py
+++ jetic_bf/jetic_bf.py
@@ -1,11 +1,11 @@
 def assign(p, a, b):
     remove_b = zero(p, b)
     p = getP(b)
-    s = "a[b+1 c+1 a-1 a] c[a+1 c-1 c] b".replace('a', a).replace('b', b).replace('c', '_')
+    s = "@a[@b+1 @c+1 @a-1 @a] @c[@a+1 @c-1 @c] @b".replace('@a', a).replace('@b', b).replace('@c', '_')
     return remove_b + jeticbf(s, p)
 
 def zero(p, a):
-    s = "a[a-1 a]".replace('a', a)
+    s = "@a[@a-1 @a]".replace('@a', a)
     return jeticbf(s, p)
 
 def getP(a):
@@ -18,15 +18,7 @@ def jeticbf(S, p = 0):
     while (i < len(S)):
         c = S[i]
         # current
-        if (c >= '_' and c <= 'z'):
-            new_p = getP(c)
-            move_p = new_p - p
-            p = new_p
-            if (move_p > 0):
-                R += ">"*move_p
-            elif (move_p < 0):
-                R += "<"*-move_p
-        elif (c == '+' or c == '-'):
+        if (c == '+' or c == '-'):
             number = ""
             i += 1
             while (S[i] >= '0' and S[i] <= '9'):
@@ -47,6 +39,16 @@ def jeticbf(S, p = 0):
             continue
         elif (c == ' '):
             pass
+        elif (c == '[' or c == ']' or c == '<' or c == '>'):
+            R += c
+        elif (c >= 'A' and c <= 'z'):
+            new_p = getP(c)
+            move_p = new_p - p
+            p = new_p
+            if (move_p > 0):
+                R += ">"*move_p
+            elif (move_p < 0):
+                R += "<"*-move_p
         else:
             R += c
         i += 1
@@ -54,7 +56,7 @@ def jeticbf(S, p = 0):
 
 def main():
     S = raw_input()
-#    S = "s+32 i+9 i[a+1 =ab b+48 b. s. i-1 i]"
+#    S = "s+32 i+99 j+10 i[a+1 j-1 ~A A-1 j[A+1 =jJ ~j j] =Jj A[~a b+1 j+9 ~A A] =bx x+48 x. =ax x+48 x. s. i-1 i]"
 #    S = "a+49 =ab b."
     print jeticbf(S)        
     
